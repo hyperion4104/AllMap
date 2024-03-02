8c826208756bb7c21ae1f3607ca4ed3311e9f3e0
hyperion4104 <joaquin.gaspar.doppelt@gmail.com>
2024-03-02 13:23:05 -0500

    markers + icons

--- map.js
+++ map.js
@@ -1,6 +1,7 @@
 let map;
-function initMap(){
-    map = new google.maps.Map(document.getElementById("map"), {
+async function initMap(){
+    const { Map } = await google.maps.importLibrary("maps");
+    const map = new google.maps.Map(document.getElementById("map"), {
         zoom: 2.5,
         center: {lat: 0, lng: 0},
         mapId:'5216e0e972dcd987',
@@ -8,4 +9,29 @@ function initMap(){
         disableDefaultUI: true,
         streetViewControl: false
     });
+    var markers=[];
+
+    const url = "https://www.gdacs.org/gdacsapi/api/events/geteventlist/EVENTS4APP";
+    const eq = "./eq.png";
+    const fl = "./fl.png";
+    const wf = "./wf.png";
+    const dr = "./dr.png";
+    const vo = "./vo.png";
+    const tc = "./tc.png";
+
+    const response = await fetch(url);
+    const data = await response.json();
+    const events = data.features;
+    count = 0;
+    for(const element of events) {
+        const cords = element.geometry.coordinates;
+        console.log(cords);
+
+        markers[count] = new google.maps.Marker({
+            map,
+            position: {lat: cords[1], lng: cords[0]},
+            icon: eq
+        });
+        count++;
+    }
 }
